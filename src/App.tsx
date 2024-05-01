20125704f9b81bb303a80c6e91a6de0c308c5edb
Omobayode6 <omobayode93@gmail.com>
2024-05-01 10:45:00 +0100

    Setup merchant site for mybalance api

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// create login screen just skeleton
+// create screen that has cart where they can oay with mybalance
+//  create screen to connect to mybalance
+
+function App() {
+
+
+  return (
+    <div>
+      <p className="text-red-600">here</p>
+    </div>
+  )
+}
+
+export default App
